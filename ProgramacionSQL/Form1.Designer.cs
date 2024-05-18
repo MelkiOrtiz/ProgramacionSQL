f67223efe83223644bae9e6ad283a6bfbb2a7c5d
Melki <mortizm28@miumg.edu.gt>
2024-05-18 12:51:17 -0600

    Agregar archivos de proyecto.

---
+++ ProgramacionSQL/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace ProgramacionSQL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonPrueba = new System.Windows.Forms.Button();
+            this.dataGridViewPersonajes = new System.Windows.Forms.DataGridView();
+            this.buttonCargar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxID = new System.Windows.Forms.TextBox();
+            this.textBoxNombre = new System.Windows.Forms.TextBox();
+            this.numericUpDownNivelPoder = new System.Windows.Forms.NumericUpDown();
+            this.buttonCrear = new System.Windows.Forms.Button();
+            this.comboBoxRaza = new System.Windows.Forms.ComboBox();
+            this.buttonBuscar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPersonajes)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownNivelPoder)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonPrueba
+            // 
+            this.buttonPrueba.Location = new System.Drawing.Point(732, 255);
+            this.buttonPrueba.Name = "buttonPrueba";
+            this.buttonPrueba.Size = new System.Drawing.Size(155, 23);
+            this.buttonPrueba.TabIndex = 0;
+            this.buttonPrueba.Text = "Prueba de conexion";
+            this.buttonPrueba.UseVisualStyleBackColor = true;
+            this.buttonPrueba.Click += new System.EventHandler(this.buttonPrueba_Click);
+            // 
+            // dataGridViewPersonajes
+            // 
+            this.dataGridViewPersonajes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewPersonajes.Location = new System.Drawing.Point(12, 242);
+            this.dataGridViewPersonajes.Name = "dataGridViewPersonajes";
+            this.dataGridViewPersonajes.RowHeadersWidth = 51;
+            this.dataGridViewPersonajes.RowTemplate.Height = 24;
+            this.dataGridViewPersonajes.Size = new System.Drawing.Size(692, 219);
+            this.dataGridViewPersonajes.TabIndex = 1;
+            // 
+            // buttonCargar
+            // 
+            this.buttonCargar.Location = new System.Drawing.Point(770, 203);
+            this.buttonCargar.Name = "buttonCargar";
+            this.buttonCargar.Size = new System.Drawing.Size(117, 28);
+            this.buttonCargar.TabIndex = 2;
+            this.buttonCargar.Text = "Cargar Datos";
+            this.buttonCargar.UseVisualStyleBackColor = true;
+            this.buttonCargar.Click += new System.EventHandler(this.buttonCargar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(55, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Nombre";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(52, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Raza";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(55, 141);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Power";
+            // 
+            // textBoxID
+            // 
+            this.textBoxID.Location = new System.Drawing.Point(161, 24);
+            this.textBoxID.Name = "textBoxID";
+            this.textBoxID.Size = new System.Drawing.Size(100, 22);
+            this.textBoxID.TabIndex = 7;
+            this.textBoxID.Leave += new System.EventHandler(this.textBoxID_Leave);
+            // 
+            // textBoxNombre
+            // 
+            this.textBoxNombre.Location = new System.Drawing.Point(161, 60);
+            this.textBoxNombre.Name = "textBoxNombre";
+            this.textBoxNombre.Size = new System.Drawing.Size(100, 22);
+            this.textBoxNombre.TabIndex = 8;
+            // 
+            // numericUpDownNivelPoder
+            // 
+            this.numericUpDownNivelPoder.Location = new System.Drawing.Point(161, 141);
+            this.numericUpDownNivelPoder.Maximum = new decimal(new int[] {
+            2147483647,
+            0,
+            0,
+            0});
+            this.numericUpDownNivelPoder.Name = "numericUpDownNivelPoder";
+            this.numericUpDownNivelPoder.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDownNivelPoder.TabIndex = 10;
+            // 
+            // buttonCrear
+            // 
+            this.buttonCrear.Location = new System.Drawing.Point(55, 185);
+            this.buttonCrear.Name = "buttonCrear";
+            this.buttonCrear.Size = new System.Drawing.Size(75, 23);
+            this.buttonCrear.TabIndex = 11;
+            this.buttonCrear.Text = "Crear";
+            this.buttonCrear.UseVisualStyleBackColor = true;
+            this.buttonCrear.Click += new System.EventHandler(this.buttonCrear_Click);
+            // 
+            // comboBoxRaza
+            // 
+            this.comboBoxRaza.FormattingEnabled = true;
+            this.comboBoxRaza.Location = new System.Drawing.Point(160, 103);
+            this.comboBoxRaza.Name = "comboBoxRaza";
+            this.comboBoxRaza.Size = new System.Drawing.Size(184, 24);
+            this.comboBoxRaza.TabIndex = 12;
+            // 
+            // buttonBuscar
+            // 
+            this.buttonBuscar.Location = new System.Drawing.Point(313, 24);
+            this.buttonBuscar.Name = "buttonBuscar";
+            this.buttonBuscar.Size = new System.Drawing.Size(75, 23);
+            this.buttonBuscar.TabIndex = 13;
+            this.buttonBuscar.Text = "Buscar";
+            this.buttonBuscar.UseVisualStyleBackColor = true;
+            this.buttonBuscar.Click += new System.EventHandler(this.buttonBuscar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(923, 473);
+            this.Controls.Add(this.buttonBuscar);
+            this.Controls.Add(this.comboBoxRaza);
+            this.Controls.Add(this.buttonCrear);
+            this.Controls.Add(this.numericUpDownNivelPoder);
+            this.Controls.Add(this.textBoxNombre);
+            this.Controls.Add(this.textBoxID);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonCargar);
+            this.Controls.Add(this.dataGridViewPersonajes);
+            this.Controls.Add(this.buttonPrueba);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPersonajes)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownNivelPoder)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonPrueba;
+        private System.Windows.Forms.DataGridView dataGridViewPersonajes;
+        private System.Windows.Forms.Button buttonCargar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxID;
+        private System.Windows.Forms.TextBox textBoxNombre;
+        private System.Windows.Forms.NumericUpDown numericUpDownNivelPoder;
+        private System.Windows.Forms.Button buttonCrear;
+        private System.Windows.Forms.ComboBox comboBoxRaza;
+        private System.Windows.Forms.Button buttonBuscar;
+    }
+}
+
---
+++ ProgramacionSQL/Form1.cs
@@ -0,0 +1,106 @@
+﻿using ProgramacionSQL.Data.DataAcces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgramacionSQL
+{
+    public partial class Form1 : Form
+    {
+        //definir clase de instancia 
+        private personajeDB personaje;
+        // Lista de razas
+        private string[] razasDragonBall = {
+    "Android",
+    "Bio-Android",
+    "Humana",
+    "Humano",
+    "Majin",
+    "Namekuseijin",
+    "Saiyajin",
+    "Saiyajin/Humano",
+    "Saiyajin/Saiyajin"
+};
+        public Form1()
+        {
+            InitializeComponent();
+            personaje = new personajeDB();
+        }
+
+        private void buttonPrueba_Click(object sender, EventArgs e)
+        {
+            if (personaje.ProbarConexion())
+            {
+                MessageBox.Show("Simoncho");
+            }
+             else
+            {
+                MessageBox.Show("Nel pastel");
+            }
+        }
+
+        private void buttonCargar_Click(object sender, EventArgs e)
+        {
+            DataTable dt = personaje.LeerPersonajes();
+            dataGridViewPersonajes.DataSource = dt;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonCrear_Click(object sender, EventArgs e)
+        {
+            string nombre = textBoxNombre.Text;
+            string raza = comboBoxRaza.Text;
+            int nivelPoder = (int)numericUpDownNivelPoder.Value;
+            int respuesta = personaje.CrearPersonaje(nombre, raza, nivelPoder);
+            if (respuesta >0)
+            {
+                MessageBox.Show("Creado con exito");
+                dataGridViewPersonajes.DataSource = personaje.LeerPersonajes();
+            } else
+            {
+                MessageBox.Show("Algo hiciste mal");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBoxRaza.Items.AddRange(razasDragonBall);
+        }
+
+        private void BuscarPorId()
+        {
+            int idPersonajeBuscar = int.Parse(textBoxID.Text);
+            DataTable personajeEncontrado = personaje.BuscarPersonajePorId(idPersonajeBuscar);
+            if (personajeEncontrado.Rows.Count > 0)
+            {
+                string nombre = personajeEncontrado.Rows[0]["nombre"].ToString();
+                string raza = personajeEncontrado.Rows[0]["raza"].ToString();
+                int niverlPoder = int.Parse(personajeEncontrado.Rows[0]["nivel_poder"].ToString());
+                textBoxNombre.Text = nombre;
+                comboBoxRaza.Text = raza;
+                numericUpDownNivelPoder.Value = niverlPoder;
+            }
+        }
+
+        private void buttonBuscar_Click(object sender, EventArgs e)
+        {
+          BuscarPorId();
+        }
+
+        private void textBoxID_Leave(object sender, EventArgs e)
+        {
+            BuscarPorId();
+        }
+    }
+}
